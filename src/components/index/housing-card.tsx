031bb5768c48cb3c0312387a30d69eabe41a2cd6
Goulven Nev <nevilain@gmail.com>
2021-08-05 10:49:30 +0200

    update housing & index

--- src/components/index/housing-card.tsx
+++ src/components/index/housing-card.tsx
@@ -1,21 +1,18 @@
 import React from "react"
-import data from "../../../assets/data/data.json"
+import { Housing } from "../../pages"
 
 /**
  * Display all the HousingCards in a section
  */
-export default function HousingCards() {
-    const housings:dataRespond = importData()
-    // If the backend worked, we got an Array<Housing>, otherwise display an error
-    if(housings instanceof Array) {
-        const housingsCards:Array<JSX.Element> = createHousingsCards(housings)
-        return (
-            <section className="m-4 p-4 rounded bg-gray-200
-                grid grid-cols-3 gap-4">
-                {housingsCards}
-            </section>
-        )
-    }else { return <h2>Désolé, une erreur est survenue 😥</h2> }
+export default function HousingCards(props:Props) {
+    const housings:Array<Housing> = props.housingData
+    const housingsCards:Array<JSX.Element> = createHousingsCards(housings)
+    return (
+        <section className="m-4 p-4 rounded bg-gray-200
+            grid grid-cols-3 gap-4">
+            {housingsCards}
+        </section>
+    )
 }
 
 /**
@@ -25,7 +22,7 @@ export default function HousingCards() {
  */
 function createHousingsCards(housings:Array<Housing>):Array<JSX.Element> {
     return housings.map((housing) =>
-        <article className="rounded-md bg-red-300 h-40">
+        <article className="rounded-md bg-red-300 h-40" key={housing.id}>
             <h1>
                 {housing.title}
             </h1>
@@ -34,33 +31,6 @@ function createHousingsCards(housings:Array<Housing>):Array<JSX.Element> {
 }
 
 /**
- * Import the housings data from the JSON
- * @returns {dataRespond} 
+ * Just a type to check is the prop is valid
  */
- function importData():dataRespond {
-    return data
-}
-
-/**
- * Currently this is pretty useless because we import the data from a JSON
- * but when we'll have a back end, this allow to accept a backend failure
- * and display an error instead of just crashing !
- * @type {dataRespond}
- */
-type dataRespond = Array<Housing> | {error: number, errorMessage: string}
-
-/**
- * Check if the housings objects are valid
- * @interface Housing
- */
-interface Housing {
-    id: string; 
-    title: string; 
-    cover: string; 
-    pictures: string[]; 
-    description: string; 
-    host: { name: string; picture: string; }; rating: string; 
-    location: string; 
-    equipments: string[]; 
-    tags: string[];
-}
+type Props = { housingData: Array<Housing> }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,38 @@
 import React from "react"
+import data from "../../assets/data/data.json"
 import HousingCards from "../components/index/housing-card"
 
 /**
  * The homepage of Kasa !
  */
 export default function IndexPage() {
+  const data:Array<Housing> = importData()
   return (
-    <HousingCards />
+    <HousingCards housingData={data} />
   )
 }
 
+/**
+ * Import the housings data from the JSON
+ * @returns {Array<Housing>} 
+ */
+ function importData():Array<Housing> {
+  return data
+}
+
+/**
+ * Check if the housings objects are valid
+ * @interface Housing
+ */
+export interface Housing {
+    id: string; 
+    title: string; 
+    cover: string; 
+    pictures: string[]; 
+    description: string; 
+    host: { name: string; picture: string; }; rating: string; 
+    location: string; 
+    equipments: string[]; 
+    tags: string[];
+}
 
