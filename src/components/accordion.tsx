61330bc4ac212896817ee27a6a94d51d78c9fc53
Goulven Nev <nevilain@gmail.com>
2021-08-17 11:57:09 +0200

    add accordion

---
+++ src/components/accordion.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useRef } from "react"
+
+/**
+ * An accordion element, with a title box, and a content
+ * who is showed only on click
+ */
+export default function Accordion(props:Props):JSX.Element {
+  const [isOpen, setIsOpen] = useState(false)
+  const [Height, setHeight] = useState("0px")
+  // useRef will be usefull to know the height of the DOM element
+  const content = useRef<HTMLDivElement>(null)
+  /**
+   * Open & close the Accordion, called by onClick() on the button
+   */
+  function toggleAccordion() {
+    // Needed by TypeScript, to be sure it's not null anymore after JSX rendering
+    if(content && content.current) {
+        setIsOpen(isOpen === false ? true : false)
+        setHeight(isOpen === true ? "0px" : content.current.scrollHeight + "px")
+    }
+  }
+
+  return (
+    <div className="my-4 bg-gray-200 rounded-lg">
+      <button className="text-left w-full bg-red-400 rounded-lg py-2 px-4 text-white text-lg" 
+        onClick={toggleAccordion}>
+        <p className="">{props.title}</p>
+      </button>
+      <div
+        ref={content}
+        style={{ maxHeight: `${Height}` }}
+        className="overflow-hidden duration-300"
+      >
+        <p className="p-4 text-red-400 text-lg">
+            {props.content}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+/**
+ * Just checking if the props are valids
+ */
+interface Props {
+    title: string,
+    content: string
+}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,40 @@
 import React from "react"
 import HeroAbout from "../components/about/hero-about"
+import Accordion from "../components/accordion"
 
 /**
- * The homepage of Kasa !
+ * About Kasa, its values and its team !
  */
 export default function AboutPage():JSX.Element {
+
+  /**
+   * Without backend, I have to hard write the content of this section
+   * it'll be better to change that later for a real backend.
+   */
+  const fiabiliteContent = `Les annonces postées sur Kasa garantissent une fiabilité totale. 
+    Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées 
+    par nos équipes.`
+  const respectContent = `La bienveillance fait partie des valeurs fondatrices de Kasa. Tout 
+    comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre 
+    plateforme.`
+  const serviceContent = `Nos équipes se tiennent à votre disposition pour vous fournir une 
+    expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.`
+  const responsabiliteContent = `La sécurité est la priorité de Kasa. Aussi bien pour nos 
+    hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos 
+    services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de 
+    vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité 
+    domestique pour nos hôtes.`
+
+
   return (
     <main className="px-4 xl:mx-auto max-w-screen-xl">
       <HeroAbout />
+      <div className="mx-auto max-w-screen-lg">
+      <Accordion title="Fiabilité" content={fiabiliteContent} />
+      <Accordion title="Respect" content={respectContent} />
+      <Accordion title="Service" content={serviceContent} />
+      <Accordion title="Responsabilité" content={responsabiliteContent} />
+      </div>
     </main>
   )
 }
