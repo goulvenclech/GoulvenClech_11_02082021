777f84a94089f07e0dbf3a88b87701148a94333c
Goulven Nev <nevilain@gmail.com>
2021-09-08 14:15:43 +0200

    fix footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ import imgUrl from "../../assets/images/logo_blanc.svg"
 export default function Footer():JSX.Element {
     return (
         <footer className="bg-black">
-            <div className="block w-60 text-white mx-auto py-12">
+            <div className="block text-white text-center mx-auto py-12">
                 <img className="mx-auto pb-4" src={imgUrl}>
                 </img>
                 © 2020 Kasa. All rights reserved 
